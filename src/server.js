fff6ddb95c3112f07e26a4289bf90c6cee7b3019
lucasbatistaa <lucas.lucas2004.30@gmail.com>
2024-08-21 20:38:20 -0300

    add environments

--- src/server.js
+++ src/server.js
@@ -1,13 +1,13 @@
 import express from 'express'
 import userRouter from './routers/userRouter.js'
 import productRouter from './routers/productRouter.js'
+import { ENVIRONMENT, HOST, PORT } from './config.js'
 
 const app = express()
-const port = 3000
 
 app.use('/user', userRouter)
 app.use('/product', productRouter)
 
-app.listen(port, () => {
-    console.log(`Server running! Port ${port}`)
+app.listen(PORT, () => {
+    console.log(`Server running! Environment ${ENVIRONMENT} in ${ENVIRONMENT === 'production' ? HOST : HOST}:${PORT}`)
 })
